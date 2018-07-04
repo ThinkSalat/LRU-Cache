68e7d382e824bb1cb8685f3000e1e5d0619d80dc
Shawn Salat <shawn.salat@gmail.com>
2018-07-03 23:02:26 -0700

    complete hash map

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -3,6 +3,7 @@ require_relative 'p04_linked_list'
 
 class HashMap
   attr_reader :count
+  include Enumerable
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
@@ -10,18 +11,34 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key) 
   end
 
   def set(key, val)
+    if include?(key) 
+      bucket(key).update(key, val)
+    else
+      bucket(key).append(key,val)
+      @count += 1
+    end
+    resize! if @count > num_buckets
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    bucket(key).remove(key)
+    @count -= 1
   end
 
   def each
+    @store.each do |bucket|
+      bucket.each do |node|
+        yield(node.key, node.val)
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
@@ -42,9 +59,19 @@ class HashMap
   end
 
   def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { LinkedList.new }
+
+    old_store.each do |bucket|
+      bucket.each do |node|
+        set(node.key, node.val)
+      end
+    end
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
