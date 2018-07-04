4d48b0856d49e1f2850952171cb0f64922bc4442
Shawn Salat <shawn.salat@gmail.com>
2018-07-04 10:21:35 -0700

    implement LRU cache

--- lib/p06_lru_cache.rb
+++ lib/p06_lru_cache.rb
@@ -15,6 +15,13 @@ class LRUCache
   end
 
   def get(key)
+    if @map.include?(key)
+      node = @map[key]
+      update_node!(node)
+      node.val
+    else
+      calc!(key)
+    end
   end
 
   def to_s
@@ -25,12 +32,23 @@ class LRUCache
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    val = @prc.call(key)
+    eject! if count >= @max
+    node = @store.append(key,val)
+    @map[key] = node
+    val
   end
 
   def update_node!(node)
     # suggested helper method; move a node to the end of the list
+    @store.remove(node.key)
+    @store.append(node.key, node.val)
   end
 
   def eject!
+    # remove tail
+    key = @store.first.key
+    @store.first.remove
+    @map.delete(key)    
   end
 end
--- lib/p07_practical.rb
+++ lib/p07_practical.rb
@@ -1,5 +1,14 @@
 require_relative 'p05_hash_map'
 
 def can_string_be_palindrome?(string) 
-  
+  hashmap = HashMap.new
+  string.chars.each do |char| 
+    hashmap[char] = hashmap[char] || 0
+    hashmap[char] += 1
+  end
+  numOdds = 0
+  hashmap.each do |k,v|
+    numOdds += 1 if v.odd?
+  end
+  return numOdds <= 1
 end
